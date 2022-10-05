d5003e8349fc68e962a903a88ca9c6f797729117
Stas Korobchenko <stanislav.govoruhin@gmail.com>
2022-10-05 21:34:38 +0300

    1 task

--- src/components/App.js
+++ src/components/App.js
@@ -5,19 +5,27 @@ import { Filter } from "./Filter/filter";
 import { Form } from "./form/Form";
 import { Container } from "./app.styled";
 
-
+const KEY = 'list_contacts';
 export class App extends Component {
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [ ],
     filter: '',
   };
 
   filterInputId = nanoid();
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem(KEY));
+    if (contacts) {
+      this.setState({ contacts });
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem(KEY, JSON.stringify(this.state.contacts));
+    }
+  }
 
   handleFilterChange = e => {
     const {name, value} = e.currentTarget
